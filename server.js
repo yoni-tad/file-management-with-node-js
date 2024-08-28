b4fc312b54b5000db0e94a552d00abef01819b0b
yoni-tad <yonitad2404@gmail.com>
2024-08-28 13:15:29 +0300

    update 08/28

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.json());
+
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB)
+        console.log("✅ Mongo db successfully connected")
+
+        await app.listen(3000, () => console.log('✅ Server start at 3000'))
+    } catch(e) {
+        console.error("❌ Server error:", e.message);
+    }
+})();
