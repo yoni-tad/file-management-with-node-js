ac0e07bcdc64e99dfa60a7d5625da0ad0f0b6733
yoni-tad <yonitad2404@gmail.com>
2024-08-31 01:57:41 +0300

    update 08/31

--- public/src/js/files.js
+++ public/src/js/files.js
@@ -23,7 +23,10 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   fetchFiles(path);
   checkUser();
-  create(path)
+  create(path);
+  document.getElementById('fileUpload').onchange = function (event) {
+    fileUpload(event, path)
+  }
   // createFolder(path, 'newFile')
 
   // Check user whether authenticated or not
@@ -223,5 +226,63 @@ document.addEventListener("DOMContentLoaded", async () => {
       console.log(`Error: ${e}`);
     }
     fetchFiles(path);
+  }
+
+  // file upload
+  async function fileUpload(e, path) {
+    e.preventDefault();
+    document.getElementById("progressForm").style.display = "block";
+    const email = localStorage.getItem("email");
+    const quota = localStorage.getItem("quota");
+
+    const fileInput = document.getElementById("fileUpload");
+    const file = fileInput.files[0];
+    const fileName = file.name;
+    const fileSize = file.size;
+    const newQuota = parseInt(quota) + fileSize;
+
+    if (fileSize > 524288000) {
+      return console.log("Max upload upto 500 MB");
+    } else if (role == "user" && newQuota > 52428800) {
+      return console.log("Your quota is full use premium" + newQuota);
+    } else if (role == "premium" && newQuota > 157286400) {
+      return console.log("Your quota is full");
+    }
+
+    const formData = new FormData();
+    formData.append("size", fileSize);
+    formData.append("path", path);
+    formData.append("email", email);
+    formData.append("file", file);
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", `${domain}/api/file/upload-folder`, true);
+    xhr.upload.onprogress = function (event) {
+      if (event.lengthComputable) {
+        const percentComplete = Math.round((event.loaded / event.total) * 100);
+        document.getElementById("fileName").innerText = fileName;
+        document.getElementById("fileSize").innerText = `${(
+          fileSize /
+          (1024 * 1024)
+        ).toFixed(2)} MB`;
+        document.getElementById(
+          "percentBar"
+        ).style.width = `${percentComplete}%`;
+        document.getElementById(
+          "percentValue"
+        ).innerText = `${percentComplete}%`;
+      }
+    };
+    xhr.onload = function () {
+      if (xhr.status === 201) {
+        fetchFiles(path);
+      } else {
+        console.log("Error:", JSON.parse(xhr.responseText).message);
+      }
+    };
+    xhr.onerror = function () {
+      console.error("Upload failed.");
+    };
+    xhr.send(formData);
+    
   }
 });
