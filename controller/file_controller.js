912c23a7e5f3929f045f07e0228b05c5fe15b7cd
yoni-tad <yonitad2404@gmail.com>
2024-08-31 17:29:36 +0300

    update 08/31

--- controller/file_controller.js
+++ controller/file_controller.js
@@ -49,19 +49,37 @@ exports.getFiles = async (req, res) => {
     const files = [];
     for (const file of fileList) {
       if (fs.lstatSync(filePath + file).isDirectory()) {
-        const folderInfo = await FolderSchema.findOne({folderName: file, email: email, folderPath: path})
-        if(!folderInfo){
-          return res.status(404).json({message: 'Folder not found!'})
+        const folderInfo = await FolderSchema.findOne({
+          folderName: file,
+          email: email,
+          folderPath: path,
+        });
+        if (!folderInfo) {
+          return res.status(404).json({ message: "Folder not found!" });
         }
-        const info = {id: folderInfo._id, type: 'folder', name: file, date: folderInfo.createdAt}
-        files.unshift(info)
+        const info = {
+          id: folderInfo._id,
+          type: "folder",
+          name: file,
+          date: folderInfo.createdAt,
+        };
+        files.unshift(info);
       } else {
-        const fileInfo = await FileSchema.findOne({fileName: file, email: email, filePath: filePath})
-        if(!fileInfo){
-          return res.status(404).json({message: 'Folder not found!'})
+        const fileInfo = await FileSchema.findOne({
+          fileName: file,
+          email: email,
+          filePath: filePath,
+        });
+        if (!fileInfo) {
+          return res.status(404).json({ message: "Folder not found!" });
         }
-        const info = {id: fileInfo._id, type: 'file', name: file, date: fileInfo.createdAt}
-        files.push(info)
+        const info = {
+          id: fileInfo._id,
+          type: "file",
+          name: file,
+          date: fileInfo.createdAt,
+        };
+        files.push(info);
       }
     }
 
@@ -104,4 +122,38 @@ exports.uploadFile = async (req, res) => {
   }
 };
 
-exports.DeleteFolder = async (req, res) => {};
+exports.DeleteFolder = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const folder = await FolderSchema.findById(id);
+    if (!folder) return res.status(404).json({ message: "Folder not found!" });
+
+    const folderPath = folder.folderPath;
+    const folderName = folder.folderName;
+    const email = folder.email;
+    const path = dir + email + folderPath + folderName;
+    fs.rmSync(path, {recursive: true, force: true})
+
+    const files = await FileSchema.find({ filePath: `${path}/` });
+    var total = 0;
+    files.forEach((file) => {
+      const fileSize = file.size;
+      total = total + fileSize;
+    });
+
+    const user = await UserSchema.findOne({ email: email });
+    const quota = user.quota;
+    const totalQuota = quota - total
+    const quotaUpdate = await UserSchema.findOneAndUpdate(
+      { email: email },
+      { quota: totalQuota }
+    );
+    const deleteFiles = await FileSchema.deleteMany({ filePath: `${path}/` })
+
+    const response = await FolderSchema.findByIdAndDelete(id)
+    res.json({ message: "Folder successfully deleted" });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
