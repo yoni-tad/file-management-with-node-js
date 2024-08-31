1d9bb2677e6df29ec2c946745f08a62c7ddef4c1
yoni-tad <yonitad2404@gmail.com>
2024-08-31 18:26:03 +0300

    update 08/31

--- public/src/js/files.js
+++ public/src/js/files.js
@@ -24,10 +24,9 @@ document.addEventListener("DOMContentLoaded", async () => {
   fetchFiles(path);
   checkUser();
   create(path);
-  document.getElementById('fileUpload').onchange = function (event) {
-    fileUpload(event, path)
-  }
-  // createFolder(path, 'newFile')
+  document.getElementById("fileUpload").onchange = function (event) {
+    fileUpload(event, path);
+  };
 
   // Check user whether authenticated or not
   async function checkUser() {
@@ -129,8 +128,40 @@ document.addEventListener("DOMContentLoaded", async () => {
           uploadByCell.textContent = `${firstName} ${lastName}`;
 
           const actionCell = document.createElement("td");
-          actionCell.innerHTML = `<div class="flex justify-center space-x-2">
-                              <button
+          if (data[i].type == "folder") {
+            actionCell.innerHTML = `<div class="flex justify-end space-x-2">
+            <button data-id=${data[i].id}
+                class="flex items-center px-4 py-1 border border-gray-800 rounded-lg hover:border-blue-400 relative group">
+                <svg class="w-4 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor">
+                    <path stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                <span
+                    class="absolute bottom-full mb-2 hidden group-hover:block px-2 py-1 text-xs text-white bg-black rounded">Edit</span>
+            </button>
+            <button data-id=${data[i].id} id="deleteFolderBtn"
+                class="flex items-center px-4 py-1 border border-gray-800 rounded-lg hover:border-red-400 relative group deleteFolderBtn">
+                <svg class="w-4 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor">
+                    <path stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+                <span
+                    class="absolute bottom-full mb-2 hidden group-hover:block px-2 py-1 text-xs text-white bg-black rounded">Delete</span>
+            </button>
+        </div>`;
+          } else {
+            actionCell.innerHTML = `<div class="flex justify-center space-x-2">
+                              <button data-id=${data[i].id}
                                   class="flex items-center px-4 py-1 border border-gray-800 rounded-lg hover:border-green-400 relative group">
                                   <svg class="w-4 h-4"
                                       xmlns="http://www.w3.org/2000/svg"
@@ -144,7 +175,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                                   <span
                                       class="absolute bottom-full mb-2 hidden group-hover:block px-2 py-1 text-xs text-white bg-black rounded">Download</span>
                               </button>
-                              <button
+                              <button data-id=${data[i].id}
                                   class="flex items-center px-4 py-1 border border-gray-800 rounded-lg hover:border-blue-400 relative group">
                                   <svg class="w-4 h-4"
                                       xmlns="http://www.w3.org/2000/svg"
@@ -158,8 +189,8 @@ document.addEventListener("DOMContentLoaded", async () => {
                                   <span
                                       class="absolute bottom-full mb-2 hidden group-hover:block px-2 py-1 text-xs text-white bg-black rounded">Edit</span>
                               </button>
-                              <button
-                                  class="flex items-center px-4 py-1 border border-gray-800 rounded-lg hover:border-red-400 relative group">
+                              <button data-id=${data[i].id} id="deleteFolderBtn"
+                                  class="flex items-center px-4 py-1 border border-gray-800 rounded-lg hover:border-red-400 relative group deleteFolderBtn">
                                   <svg class="w-4 h-4"
                                       xmlns="http://www.w3.org/2000/svg"
                                       fill="none" viewBox="0 0 24 24"
@@ -173,6 +204,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                                       class="absolute bottom-full mb-2 hidden group-hover:block px-2 py-1 text-xs text-white bg-black rounded">Delete</span>
                               </button>
                           </div>`;
+          }
 
           row.appendChild(nameCell);
           row.appendChild(dateCell);
@@ -204,6 +236,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       });
   }
 
+  // Create folder
   async function createFolder(path, name) {
     try {
       const email = localStorage.getItem("email");
@@ -287,6 +320,34 @@ document.addEventListener("DOMContentLoaded", async () => {
       console.error("Upload failed.");
     };
     xhr.send(formData);
-    
+  }
+
+  // Delete folder
+  // delete folders and files
+  document.querySelector("#example tbody").addEventListener("click", (e) => {
+    if (e.target.classList.contains("deleteFolderBtn")) {
+      const id = e.target.getAttribute("data-id");
+      document.getElementById("deleteModal").style.display = "block";
+      const deleteModalBtn = document.getElementById("deleteModalBtn");
+      deleteModalBtn.setAttribute("data-id", id);
+    }
+  });
+
+  document.getElementById("deleteModalBtn").addEventListener("click", (e) => {
+    const id = e.target.getAttribute("data-id");
+    deleteFolder(id);
+  });
+
+  async function deleteFolder(id) {
+    try {
+      const response = await fetch(`${domain}/api/file/delete-folder/${id}`);
+
+      if (response.ok) {
+        console.log("Delete folder: " + id);
+        window.location.href = "files.html";
+      }
+    } catch (e) {
+      console.log(`Error: ${e}`);
+    }
   }
 });
