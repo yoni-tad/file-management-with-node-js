5377698bbbd89ba49873755944c9cac11985aa19
yoni-tad <yonitad2404@gmail.com>
2024-09-02 08:30:47 +0300

    update 09/02

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const domain = "http://localhost:3000";
   const token = localStorage.getItem("token");
+  toastr.options = {
+    closeButton: true,
+    debug: false,
+    newestOnTop: false,
+    progressBar: true,
+    positionClass: "toast-top-right",
+    preventDuplicates: false,
+    onclick: null,
+    showDuration: "300",
+    hideDuration: "1000",
+    timeOut: "3000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut",
+  };
+  toastr.success('data.message')
 
   checkUser();
   fetchFile();
